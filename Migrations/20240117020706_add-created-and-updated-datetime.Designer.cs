6ffa30608816b517a1e0830e048c237cef8d653e
zulfanfahreza <mzulfanfahreza@gmail.com>
2024-01-17 09:19:50 +0700

    Add CreatedAt and UpdatedAt on ToDoItemModel

--- Migrations/20240117020706_add-created-and-updated-datetime.Designer.cs
+++ Migrations/20240117020706_add-created-and-updated-datetime.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoListApp.DatabaseContext;
 
@@ -10,9 +11,11 @@ using ToDoListApp.DatabaseContext;
 namespace ToDoListApp.Migrations
 {
     [DbContext(typeof(ToDoDbContext))]
-    partial class ToDoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240117020706_add-created-and-updated-datetime")]
+    partial class addcreatedandupdateddatetime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
